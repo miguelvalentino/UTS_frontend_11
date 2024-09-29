c5a51a03ade031d997b03cd820b2894ebe06c837
miguelvalentino <miguelvalentiono@gmail.com>
2024-09-29 21:37:43 +0700

    tambah article + edit

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//tombol buat balik keatas
 $(document).ready(function(){
     $(window).scroll(function(){
         if ($(this).scrollTop() > 100) {
@@ -8,7 +9,7 @@ $(document).ready(function(){
     });
 
     $('#back-to-top').click(function(){
-        $('html, body').animate({scrollTop: 0}, 600);
+        $('html, body').animate({scrollTop: 0}, 0);
         return false;
     });
 });
